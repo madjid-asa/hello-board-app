e79ebcc390ad52aca043a247a342f107cbeb316b
madjid-asa <madjid.asa@gmail.com>
2019-05-14 09:59:02 +0200

    add env

---
+++ server/config/environment.js
@@ -0,0 +1,12 @@
+const morgan      = require('morgan');
+const compression = require('compression');
+const serveStatic = require('serve-static');
+
+module.exports = function(app, express){
+    app.set('port', process.env.PORT || 3000);
+    app.use(morgan('dev'));
+    app.use(compression());
+    app.use(serveStatic(`../${__dirname}/public`));
+    const expressWs   = require('express-ws')(app);
+
+};
